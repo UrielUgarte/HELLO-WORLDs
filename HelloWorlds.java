a207f03bad8fcdfbbf31281001ce719137bd9c5e
UrielUgarte <uugarte0@email.cpcc.edu>
2024-05-31 10:20:15 -0400

    New Text Added
    Added a new line of text

--- HelloWorlds.java
+++ HelloWorlds.java
@@ -2,7 +2,10 @@ public class HelloWorlds
 {
    public static void main(String[] args)
    {
-      System.out.print("Hello World!");
+      System.out.println("Hello World!");
+      
+      System.out.print("Program modified, but able to revert to original" + 
+                                              "version using GitHub Desktop.");
    
    }
 }
