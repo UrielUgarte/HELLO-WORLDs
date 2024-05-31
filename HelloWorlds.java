128082c0c1018809855429cda7f06a5de04aad3f
UrielUgarte <uugarte0@email.cpcc.edu>
2024-05-31 10:24:35 -0400

    Revert "New Text Added"
    This reverts commit a207f03bad8fcdfbbf31281001ce719137bd9c5e.

--- HelloWorlds.java
+++ HelloWorlds.java
@@ -2,10 +2,7 @@ public class HelloWorlds
 {
    public static void main(String[] args)
    {
-      System.out.println("Hello World!");
-      
-      System.out.print("Program modified, but able to revert to original" + 
-                                              "version using GitHub Desktop.");
+      System.out.print("Hello World!");
    
    }
 }
